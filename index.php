fab988a3d56c228685d2e7a21344b24c3d965570
Mr Erick <36708000+rickxy@users.noreply.github.com>
2022-11-25 17:41:53 +0300

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,6 @@
             </div>
         </div>
     </section>
-
     <!-- Javascript -->
     <script src="assets/js/vendor/jquery-2.2.4.min.js"></script>
     <script src="assets/js/vendor/bootstrap-4.1.3.min.js"></script>
