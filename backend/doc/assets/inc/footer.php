ab222cf09bce29b5345649f9a96efab71d6a3721
NetGo+ Dev <36708000+rickxy@users.noreply.github.com>
2022-12-07 21:18:51 +0300

    Update footer.php

--- backend/doc/assets/inc/footer.php
+++ backend/doc/assets/inc/footer.php
@@ -2,7 +2,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-12">
-                2020 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. Developed By Martin Mbithi Nzilani</a>
+                2020 - <?php echo date('Y'); ?> &copy; Hospital Management Information System. </a>
             </div>
 
         </div>
